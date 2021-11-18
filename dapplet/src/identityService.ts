c3dd8542f0eaa20749a29834fd8e145ccd827746
Alexander Sakhaev <alsakhaev@gmail.com>
2021-11-19 01:13:39 +0300

    feat: linking accounts

---
+++ dapplet/src/identityService.ts
@@ -0,0 +1,29 @@
+type NearAccount = string;
+type ExternalAccount = string;
+
+export class IdentityService {
+  private contract = Core.contract('near', 'app.tipping.testnet', {
+    viewMethods: ['getNearAccount', 'getMinStakeAmount'],
+    changeMethods: ['requestVerification'],
+  });
+
+  private _nearByExternal = new Map<ExternalAccount, NearAccount>();
+
+  public async getNearAccount(externalAccount: ExternalAccount): Promise<NearAccount> {
+    const contract = await this.contract;
+
+    // caching requests
+    if (!this._nearByExternal.has(externalAccount)) {
+      this._nearByExternal.set(externalAccount, contract.getNearAccount({ externalAccount }));
+    }
+
+    return this._nearByExternal.get(externalAccount);
+  }
+
+  public async requestVerification(externalAccount: ExternalAccount, isUnlink: boolean, url: string): Promise<void> {
+    const contract = await this.contract;
+    const stake = await contract.getMinStakeAmount();
+    const requestId = await contract.requestVerification({ externalAccount, isUnlink, url }, undefined, stake);
+    console.log('requestId', requestId)
+  }
+}
--- dapplet/src/index.ts
+++ dapplet/src/index.ts
@@ -1,91 +1,185 @@
-import { } from '@dapplets/dapplet-extension';
+import {} from '@dapplets/dapplet-extension';
 import WHITE_ICON from './icons/money-twiter-light.svg';
 import DARK_ICON from './icons/money-twiter-dark.svg';
+import NEAR_DARK_ICON from './icons/near-dark.svg';
 import { Tweet } from '../../overlay/src/interfaces';
 import { PaymentRepository } from './PaymentRepository';
 import { TippingsRepository } from './TippingsRepository';
 import { TippingService } from './tippingService';
+import { IdentityService } from './identityService';
 
 const { parseNearAmount, formatNearAmount } = Core.near.utils.format;
 
 @Injectable
 export default class TwitterFeature {
-
   @Inject('twitter-adapter.dapplet-base.eth')
   public adapter: any;
 
   private paymentRepository = new PaymentRepository();
   private tippingsRepository = new TippingsRepository();
   private tippingService = new TippingService(this.tippingsRepository, this.paymentRepository);
+  private identityService = new IdentityService();
 
-  private _overlay = Core.overlay({ name: 'overlay', title: 'Tipping Near' })
-    .listen({
-      getAllUserStat: () =>
-        this.tippingService.getAllUserStat()
-          .then(x => this._overlay.send('getAllUserStat_done', x))
-          .catch(e => this._overlay.send('getAllUserStat_undone', e)),
-      donateToUser: (_: any, { type, message }: any) =>
-        this.tippingService.donateToUser(message.nearAccountId, message.donateAmount)
-          .then(() => this._overlay.send('donateToUser_done'))
-          .catch(e => this._overlay.send('donateToUser_undone', e)),
-    })
+  private _step: number;
+  private _network: string;
+
+  private _overlay = Core.overlay({ name: 'overlay', title: 'Tipping Near' }).listen({
+    getAllUserStat: () =>
+      this.tippingService
+        .getAllUserStat()
+        .then((x) => this._overlay.send('getAllUserStat_done', x))
+        .catch((e) => this._overlay.send('getAllUserStat_undone', e)),
+    donateToUser: (_: any, { type, message }: any) =>
+      this.tippingService
+        .donateToUser(message.nearAccountId, message.donateAmount)
+        .then(() => this._overlay.send('donateToUser_done'))
+        .catch((e) => this._overlay.send('donateToUser_undone', e)),
+  });
 
   async activate(): Promise<void> {
-    const step = await Core.storage.get('step');
-    const network = await Core.storage.get('network');
+    this._step = await Core.storage.get('step');
+    this._network = await Core.storage.get('network');
 
-    if (!(network === 'mainnet' || network === 'testnet')) {
-      throw new Error('Only "mainnet" and "testnet" networks are supported. '
-        + 'Change the network parameter in the dapplet settings.');
+    if (this._step <= 0) {
+      throw new Error(
+        'A donation step must be more than zero. ' + 'Change the step parameter in the dapplet settings.',
+      );
     }
 
-    if (step <= 0) {
-      throw new Error('A donation step must be more than zero. '
-        + 'Change the step parameter in the dapplet settings.');
+    if (!(this._network === 'mainnet' || this._network === 'testnet')) {
+      throw new Error(
+        'Only "mainnet" and "testnet" networks are supported. ' +
+          'Change the network parameter in the dapplet settings.',
+      );
     }
 
     Core.onAction(() => this._overlay.send(''));
 
-    const { button } = this.adapter.exports;
+    const { button, avatarBadge } = this.adapter.exports;
+
     this.adapter.attachConfig({
-      POST: (tweet: Tweet) =>
+      PROFILE: (profile: any) => [
+        button({
+          DEFAULT: {
+            hidden: true,
+            init: this.onProfileButtonDefaultInit,
+          },
+          LINK: {
+            img: NEAR_DARK_ICON,
+            label: 'Link Account',
+            tooltip: 'Link account with NEAR wallet',
+            exec: this.onProfileButtonLinkExec,
+          },
+          UNLINK: {
+            img: NEAR_DARK_ICON,
+            label: 'Unlink Account',
+            tooltip: 'Unlink account from NEAR wallet',
+            exec: this.onProfileButtonUnlinkExec,
+          },
+        }),
+        avatarBadge({
+          DEFAULT: {
+            img: NEAR_DARK_ICON,
+            horizontal: 'right',
+            vertical: 'bottom',
+            hidden: true,
+            init: this.onProfileAvatarBadgeInit,
+            exec: this.onProfileAvatarBadgeExec,
+          },
+        }),
+      ],
+      POST: () => [
         button({
           DEFAULT: {
             img: { DARK: WHITE_ICON, LIGHT: DARK_ICON },
             label: 'Tip',
             tooltip: 'Send donation',
             hidden: true,
-            init: async (_, me) => {
-              const nearId = this.parseNearId(tweet.authorFullname, network);
-              if (nearId) {
-                me.hidden = false;
-                me.nearId = nearId;
-                me.label = await this.getDonationsLabel(tweet.id);
-              }
-            },
-            exec: async (_, me) => {
-              await this.tippingService.addDonation(me.nearId, tweet.id, step);
-              me.label = await this.getDonationsLabel(tweet.id);
-              if (this._overlay.isOpen()) this._overlay.send('updated');
-            },
-          }
+            init: this.onPostButtonInit,
+            exec: this.onPostButtonExec,
+          },
         }),
+        avatarBadge({
+          DEFAULT: {
+            img: NEAR_DARK_ICON,
+            horizontal: 'right',
+            vertical: 'bottom',
+            hidden: true,
+            init: this.onPostAvatarBadgeInit,
+            exec: this.onPostAvatarBadgeExec,
+          },
+        }),
+      ],
     });
   }
 
+  onProfileButtonDefaultInit = async (ctx, me) => {
+    const user = this.adapter.getCurrentUser();
+    const isMyProfile = ctx.id === user.username;
+    if (isMyProfile) {
+      const nearAccount = await this.identityService.getNearAccount('twitter/' + ctx.id);
+      me.state = nearAccount ? "UNLINK" : "LINK";
+    }
+  };
+
+  onProfileButtonLinkExec = (ctx, me) => {
+    this.identityService.requestVerification(`twitter/${ctx.id}`, false, 'https://twitter.com/' + ctx.id);
+  };
+
+  onProfileButtonUnlinkExec = (ctx, me) => {
+    this.identityService.requestVerification(`twitter/${ctx.id}`, true, 'https://twitter.com/' + ctx.id);
+  };
+
+  onProfileAvatarBadgeInit = async (ctx, me) => {
+    const nearAccount = await this.identityService.getNearAccount('twitter/' + ctx.id);
+    if (nearAccount) {
+      me.hidden = false;
+      me.tooltip = nearAccount;
+      me.nearAccount = nearAccount;
+    }
+  };
+
+  onProfileAvatarBadgeExec = (ctx, me) => {
+    window.open(`https://explorer.testnet.near.org/accounts/${me.nearAccount}`, '_blank');
+  };
+
+  onPostButtonInit = async (tweet, me) => {
+    const nearId = this.parseNearId(tweet.authorFullname, this._network);
+    if (nearId) {
+      me.hidden = false;
+      me.nearId = nearId;
+      me.label = await this.getDonationsLabel(tweet.id);
+    }
+  };
+
+  onPostButtonExec = async (tweet, me) => {
+    await this.tippingService.addDonation(me.nearId, tweet.id, this._step);
+    me.label = await this.getDonationsLabel(tweet.id);
+    if (this._overlay.isOpen()) this._overlay.send('updated');
+  };
+
+  onPostAvatarBadgeInit = async (ctx, me) => {
+    const nearAccount = await this.identityService.getNearAccount('twitter/' + ctx.authorUsername);
+    if (nearAccount) {
+      me.hidden = false;
+      me.tooltip = nearAccount;
+      me.nearAccount = nearAccount;
+    }
+  };
+
+  onPostAvatarBadgeExec = (ctx, me) => {
+    window.open(`https://explorer.testnet.near.org/accounts/${me.nearAccount}`, '_blank');
+  };
+
   async getDonationsLabel(tweetId: string): Promise<string> {
     const totalTweetDonation = await this.tippingService.getTotalDonationByTweet(tweetId);
-    return (totalTweetDonation)
-      ? formatNearAmount(parseNearAmount(totalTweetDonation.toString()), 4) + ' NEAR'
-      : 'Tip';
+    return totalTweetDonation ? formatNearAmount(parseNearAmount(totalTweetDonation.toString()), 4) + ' NEAR' : 'Tip';
   }
 
   parseNearId(authorFullname: string, network: string): string | null {
     const regExpMainnet = /(([a-z\d]+[-_])*[a-z\d]+\.)*([a-z\d]+[-_])*[a-z\d]+\.near/;
     const regExpTestnet = /(([a-z\d]+[-_])*[a-z\d]+\.)*([a-z\d]+[-_])*[a-z\d]+\.testnet/;
-    const nearId = authorFullname
-      .toLowerCase()
-      .match(network === 'testnet' ? regExpTestnet : regExpMainnet);
+    const nearId = authorFullname.toLowerCase().match(network === 'testnet' ? regExpTestnet : regExpMainnet);
 
     return nearId && nearId[0];
   }
