871941ae9159a6308c78c4f09c518c762756f370
Alexander Sakhaev <alsakhaev@gmail.com>
2021-11-21 14:24:18 +0300

    feat: added clear storage function

--- contract/assembly/index.ts
+++ contract/assembly/index.ts
@@ -292,6 +292,28 @@ export function shutdown(): void {
   logging.log("Shutdown occured");
 }
 
+export function clearStorage(): void {
+  _onlyOwner();
+  
+  storage.delete(OWNER_ACCOUNT_KEY);
+  storage.delete(ORACLE_ACCOUNT_KEY);
+  storage.delete(MIN_STAKE_AMOUNT_KEY);
+  storage.delete(INIT_CONTRACT_KEY);
+  storage.delete(ACTIVE_CONTRACT_KEY);
+
+  externalByNear.clear();
+  nearByExternal.clear();
+  pendingRequests.clear();
+  totalTipsByItem.clear();
+  totalTipsByExternal.clear();
+  availableTipsByExternal.clear();
+  approvedRequests.clear();
+
+  for (let i = 0; i < verificationRequests.length; i++) {
+    verificationRequests.pop();    
+  }
+}
+
 // HELPERS
 
 function _onlyOracle(): void {
