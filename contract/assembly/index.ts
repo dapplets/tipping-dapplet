1090c2609c0c82c1d547ad9d21f223f7b528d891
Alexander Sakhaev <alsakhaev@gmail.com>
2021-11-21 13:27:07 +0300

    feat(contract): emergency shutdown

--- contract/assembly/index.ts
+++ contract/assembly/index.ts
@@ -16,6 +16,8 @@ type ExternalAccount = string; // example: twitter/user
 
 // Common
 const OWNER_ACCOUNT_KEY = "a";
+const INIT_CONTRACT_KEY = "l";
+const ACTIVE_CONTRACT_KEY = "m";
 
 // Identity
 
@@ -49,10 +51,14 @@ const availableTipsByExternal = new PersistentUnorderedMap<ExternalAccount, u128
 // INITIALIZATION
 
 export function initialize(ownerAccountId: NearAccount, oracleAccountId: NearAccount, minStakeAmount: u128): void {
-  assert(storage.get<NearAccount>(OWNER_ACCOUNT_KEY) == null, "Already initialized contract");
-  storage.set(OWNER_ACCOUNT_KEY, ownerAccountId);
-  storage.set(ORACLE_ACCOUNT_KEY, oracleAccountId);
+  assert(storage.getPrimitive<bool>(INIT_CONTRACT_KEY, false) == false, "Contract already initialized");
+
+  storage.set<NearAccount>(OWNER_ACCOUNT_KEY, ownerAccountId);
+  storage.set<NearAccount>(ORACLE_ACCOUNT_KEY, oracleAccountId);
   storage.set<u128>(MIN_STAKE_AMOUNT_KEY, minStakeAmount);
+  storage.set<bool>(INIT_CONTRACT_KEY, true);
+  storage.set<bool>(ACTIVE_CONTRACT_KEY, true);
+
   logging.log(
     "Init contract with owner: " +
       ownerAccountId +
@@ -68,37 +74,45 @@ export function initialize(ownerAccountId: NearAccount, oracleAccountId: NearAcc
 // Identity
 
 export function getExternalAccount(nearAccount: NearAccount): ExternalAccount | null {
+  _active();
   return externalByNear.get(nearAccount);
 }
 
 export function getNearAccount(externalAccount: ExternalAccount): NearAccount | null {
+  _active();
   return nearByExternal.get(externalAccount);
 }
 
 export function getOwnerAccount(): NearAccount | null {
+  _active();
   return storage.get<NearAccount>(OWNER_ACCOUNT_KEY);
 }
 
 export function getOracleAccount(): NearAccount | null {
+  _active();
   return storage.get<NearAccount>(ORACLE_ACCOUNT_KEY);
 }
 
 export function getMinStakeAmount(): u128 {
+  _active();
   return storage.get<u128>(MIN_STAKE_AMOUNT_KEY, u128.Zero) as u128;
 }
 
 // Requests
 
 export function getPendingRequests(): u32[] {
+  _active();
   return pendingRequests.values();
 }
 
 export function getVerificationRequest(id: u32): VerificationRequest | null {
+  _active();
   if (!verificationRequests.containsIndex(id)) return null;
   return verificationRequests[id];
 }
 
 export function getRequestStatus(id: u32): u8 {
+  _active();
   if (!verificationRequests.containsIndex(id)) {
     return u8(0); // not found
   } else if (pendingRequests.has(id)) {
@@ -113,14 +127,17 @@ export function getRequestStatus(id: u32): u8 {
 // Tipping
 
 export function getTotalTipsByItemId(itemId: string): u128 {
+  _active();
   return totalTipsByItem.get(itemId, u128.Zero)!;
 }
 
 export function getTotalTipsByExternalAccount(externalAccount: ExternalAccount): u128 {
+  _active();
   return totalTipsByExternal.get(externalAccount, u128.Zero)!;
 }
 
 export function getAvailableTipsByExternalAccount(externalAccount: ExternalAccount): u128 {
+  _active();
   return availableTipsByExternal.get(externalAccount, u128.Zero)!;
 }
 
@@ -129,6 +146,7 @@ export function getAvailableTipsByExternalAccount(externalAccount: ExternalAccou
 // Identity
 
 export function approveRequest(requestId: u32): void {
+  _active();
   _onlyOracle();
   assert(verificationRequests.containsIndex(requestId), "Non-existent request ID");
   assert(pendingRequests.has(requestId), "The request has already been processed");
@@ -139,7 +157,6 @@ export function approveRequest(requestId: u32): void {
     nearByExternal.delete(req.externalAccount);
     logging.log("Accounts " + req.nearAccount + " and " + req.externalAccount + " are unlinked");
   } else {
-    
     externalByNear.set(req.nearAccount, req.externalAccount);
     nearByExternal.set(req.externalAccount, req.nearAccount);
     logging.log("Accounts " + req.nearAccount + " and " + req.externalAccount + " are linked");
@@ -150,6 +167,7 @@ export function approveRequest(requestId: u32): void {
 }
 
 export function rejectRequest(requestId: u32): void {
+  _active();
   _onlyOracle();
   assert(verificationRequests.containsIndex(requestId), "Non-existent request ID");
   assert(pendingRequests.has(requestId), "The request has already been processed");
@@ -157,24 +175,28 @@ export function rejectRequest(requestId: u32): void {
 }
 
 export function changeOwnerAccount(newAccountId: NearAccount): void {
+  _active();
   _onlyOwner();
   storage.set(OWNER_ACCOUNT_KEY, newAccountId);
   logging.log("Changed owner: " + newAccountId);
 }
 
 export function changeOracleAccount(newAccountId: NearAccount): void {
+  _active();
   _onlyOwner();
   storage.set(ORACLE_ACCOUNT_KEY, newAccountId);
   logging.log("Changed oracle: " + newAccountId);
 }
 
 export function changeMinStake(minStakeAmount: u128): void {
+  _active();
   _onlyOwner();
   storage.set<u128>(MIN_STAKE_AMOUNT_KEY, minStakeAmount);
   logging.log("Changed min stake: " + minStakeAmount.toString());
 }
 
 export function unlinkAll(): void {
+  _active();
   _onlyOwner();
   externalByNear.clear();
   nearByExternal.clear();
@@ -183,6 +205,8 @@ export function unlinkAll(): void {
 // Requests
 
 export function requestVerification(externalAccount: ExternalAccount, isUnlink: boolean, url: string): u32 {
+  _active();
+
   assert(Context.sender == Context.predecessor, "Cross-contract calls is not allowed");
   assert(
     u128.ge(Context.attachedDeposit, storage.get<u128>(MIN_STAKE_AMOUNT_KEY, u128.Zero)!),
@@ -196,7 +220,7 @@ export function requestVerification(externalAccount: ExternalAccount, isUnlink: 
   } else {
     assert(!externalByNear.contains(Context.sender), "The NEAR account already has a linked account");
     assert(!nearByExternal.contains(externalAccount), "The external account already has a linked account");
-  }    
+  }
 
   const id = verificationRequests.push(new VerificationRequest(Context.sender, externalAccount, isUnlink, url));
   pendingRequests.add(id);
@@ -212,6 +236,8 @@ export function requestVerification(externalAccount: ExternalAccount, isUnlink: 
 // Tipping
 
 export function sendTips(recipientExternalAccount: ExternalAccount, itemId: string): void {
+  _active();
+
   const nearAccount = nearByExternal.get(recipientExternalAccount);
 
   if (nearAccount) {
@@ -246,6 +272,8 @@ export function sendTips(recipientExternalAccount: ExternalAccount, itemId: stri
 }
 
 export function claimTokens(): void {
+  _active();
+
   const externalAccount = externalByNear.get(Context.sender);
   assert(externalAccount != null, "You don't have any linked account.");
 
@@ -258,6 +286,12 @@ export function claimTokens(): void {
   logging.log(Context.sender + " claimed " + availableTips.toString() + " NEAR from " + externalAccount!);
 }
 
+export function shutdown(): void {
+  _onlyOwner();
+  storage.set<bool>(ACTIVE_CONTRACT_KEY, false);
+  logging.log("Shutdown occured");
+}
+
 // HELPERS
 
 function _onlyOracle(): void {
@@ -266,4 +300,8 @@ function _onlyOracle(): void {
 
 function _onlyOwner(): void {
   assert(storage.get<NearAccount>(OWNER_ACCOUNT_KEY) == Context.sender, "Only owner account can write");
+}
+
+function _active(): void {
+  assert(storage.getPrimitive<bool>(ACTIVE_CONTRACT_KEY, false) == true, "Contract inactive");
 }
