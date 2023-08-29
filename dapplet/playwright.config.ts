d8e7259a6987ff867034d9dae49d25617d22f936
Alexander Sakhaev <alsakhaev@gmail.com>
2023-08-29 12:23:20 +0300

    test: run test registry for pull requests

--- dapplet/playwright.config.ts
+++ dapplet/playwright.config.ts
@@ -4,12 +4,14 @@ import { BrowserOptions, DappletExecutor, RegistryTypes } from '@dapplets/dapple
 
 dotenv.config();
 
+const { GIT_BRANCH_NAME, CI } = process.env;
+
 export default defineConfig<BrowserOptions & DappletExecutor.DappletExecutorOptions>({
   testDir: './tests',
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : 1,
+  forbidOnly: !!CI,
+  retries: CI ? 2 : 0,
+  workers: CI ? 1 : 1,
   reporter: 'line',
   use: {
     trace: 'on-first-retry',
@@ -19,12 +21,9 @@ export default defineConfig<BrowserOptions & DappletExecutor.DappletExecutorOpti
       name: 'chromium',
       use: {
         ...devices['Desktop Chrome'],
-        newHeadless: process.env.CI ? true : false,
+        newHeadless: CI ? true : false,
         extensionVersion: 'latest',
-        registry:
-          process.env.GIT_BRANCH_NAME === 'develop' || process.env.GIT_BRANCH_NAME === 'testing'
-            ? RegistryTypes.Test
-            : RegistryTypes.Prod,
+        registry: ['main', 'master'].includes(GIT_BRANCH_NAME) ? RegistryTypes.Prod : RegistryTypes.Test,
         devServerUrl: 'http://localhost:3001/dapplet.json',
         dappletName: 'tipping-near-dapplet',
       },
@@ -33,6 +32,6 @@ export default defineConfig<BrowserOptions & DappletExecutor.DappletExecutorOpti
   webServer: {
     command: 'npm run start',
     port: 3001,
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: !CI,
   },
 });
